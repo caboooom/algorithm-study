1f018a04faec32077c390bf17837715eda5190b1
qaw <osshun21@gmail.com>
2024-11-13 10:03:35 +0900

    [미정] 241114 - 1, 2, 3 더하기

--- BOJ/9095. 1,2,3 더하기/qaw302_241113.java
+++ BOJ/9095. 1,2,3 더하기/qaw302_241113.java
@@ -2,7 +2,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-
+/**
+ * 소요시간 : 44분
+ */
 public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
