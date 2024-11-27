ff7cfb2f41a6e11f47ff195d63e69b43235a62c4
qaw <osshun21@gmail.com>
2024-11-27 16:53:23 +0900

    [미정] 241127 가장 큰 증가하는 부분 수열
    가장 긴 증가하는 부분 수열 로직에서 길이가 하닌 합을 비교하는 방식으로 바꿔서 품

---
+++ BOJ/11055. 가장 큰 증가하는 부분 수열/qaw302_241127.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+/**
+ * 소요시간 : 11분 (가장 긴 감소/증가하는 부분 수열 푼 직후에 품)
+ */
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] A = new int[n+1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=1; i<=n; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int result = 0;
+        int[] dp = new int[n+1];
+        for (int i=1; i<=n; i++) {
+            dp[i] = A[i];
+            for (int j=1; j<i; j++) {
+                if (A[j] < A[i] && dp[j]+A[i] > dp[i]) {
+                    dp[i] = dp[j]+A[i];
+                }
+            }
+            if (result < dp[i]) {
+                result = dp[i];
+            }
+        }
+        
+        System.out.println(result);
+        br.close();
+    }
+}
