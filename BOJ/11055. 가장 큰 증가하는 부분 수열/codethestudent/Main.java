1446bec9473f0346f1862fe504de99774fbcfdb9
codeTheStudent <hjun303@gnu.ac.kr>
2024-12-01 00:58:48 +0900

    제출

---
+++ BOJ/11055. 가장 큰 증가하는 부분 수열/codethestudent/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+        int[] I = new int[N + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            I[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[1] = I[1]; //초항처리
+        for (int i = 1; i <= N; i++) {
+            dp[i] = I[i]; // 디버깅하면서 알아가도 좋음.왜 이문장이 들어가야하는지.
+            for (int j = 1; j < i; j++) {
+                if (I[i] > I[j]) {
+                    dp[i] = Math.max(dp[j] + I[i], dp[i]);
+                }
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int i : dp) {
+            max = Math.max(i, max);
+        }
+        System.out.println(max);
+
+    }
+}
