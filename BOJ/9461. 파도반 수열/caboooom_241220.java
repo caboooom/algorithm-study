b516cd0985afe21cf81a2f3e384e9446a80718ca
sylim <sammys27@naver.com>
2024-12-20 16:19:32 +0900

    [세연] 파도반 수열 풀이 추가
    5분

---
+++ BOJ/9461. 파도반 수열/caboooom_241220.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(reader.readLine());
+
+        long[] dp = new long[101];
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        for (int i = 4; i <= 100; i++) {
+            dp[i] = dp[i - 3] + dp[i - 2];
+        }
+
+        for (int t = 0; t < T; t++) {
+            int n = Integer.parseInt(reader.readLine());
+            System.out.println(dp[n]);
+        }
+    }
+}
