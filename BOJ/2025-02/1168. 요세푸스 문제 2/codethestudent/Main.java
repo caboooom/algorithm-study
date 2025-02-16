afea1d4fa291c4033dea2cbe894a90e05e157234
codeTheStudent <hjun303@gnu.ac.kr>
2025-02-16 17:04:32 +0900

    settings.json added

--- BOJ/2025-02/1168. 요세푸스 문제 2/codethestudent/Main.java
+++ BOJ/2025-02/1168. 요세푸스 문제 2/codethestudent/Main.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
